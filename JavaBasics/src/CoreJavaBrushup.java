b17d2815c14c07cb2fa94d9005d15d4816d90eaf
reema <reema.kuwar2309@gmail.com>
2025-09-10 12:28:27 +0530

    commit on branch

---
+++ JavaBasics/src/CoreJavaBrushup.java
@@ -0,0 +1,19 @@
+
+public class CoreJavaBrushup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3,5,6};
+		for (int i =0; i < arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
+		String[] name = {"abcd", "efgh", "ijkl"};
+		for (int i =0; i < name.length; i++)
+		{
+			System.out.print(name[i]);
+		}
+	}
+
+}
